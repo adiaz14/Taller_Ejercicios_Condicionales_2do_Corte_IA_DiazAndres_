32be4e13f39472d747843fbd125a1dc5ebfe0ef7
Andres Diaz <adiaz14@live.com>
2021-09-07 20:27:40 -0500

    Primer Commit

---
+++ Taller_Ejercicios_Condicionales_2do_Corte_IA_DiazAndres_.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep  7 20:22:01 2021
+
+@author: Andrés Díaz
+"""
+
+# Realizar los algoritmos que den solución a la problemática presentada
+# en los siguientes ejercicios:
+    
